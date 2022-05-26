0041e882b0154da282d9e4da1fe171c689dea904
John Lee <john3374@gmail.com>
2022-05-26 03:30:03 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const OBSWebSocket = require('obs-websocket-js');
 
 const obs = new OBSWebSocket();
-obs.connect('ws://192.168.2.189:4444', 'u8g$A{A9').then(() => { }).catch(err => console.log(err));
+obs.connect({ address: '192.168.2.189:4444', password: 'u8g$A{A9' }).then(() => {
+  obs.send('TriggerHotkeyByName', { hotkeyName: 'OBSBasic.SelectedSourceScreenshot' })
+    .then(() => console.log('hi'))
+    .catch(err => console.log(err))
+    .finally(() => obs.disconnect());
+}).catch(err => console.log(err));
