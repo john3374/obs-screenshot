2372a03ab97b8eabd5fc1b64265a4e5426323074
John <john@akfn.net>
2024-07-25 23:55:59 -0400

    updated: package and function

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
 require('dotenv').config();
 const cron = require('node-cron');
-const { WebClient } = require('@slack/web-api');
-const OBSWebSocket = require('obs-websocket-js');
-const token = process.env.SLACK_TOKEN;
-const web = new WebClient(token);
+const OBSWebSocket = require('obs-websocket-js').default;
 
-const errorHandler = (err) => web.chat.postMessage({ channel: 'C03GZMPBD4M', text: err.toString() });
 const takeScreenshot = () => {
   const obs = new OBSWebSocket();
-  obs.connect({ address: '192.168.2.189:4444', password: process.env.OBS_PASSWORD }).then(() => {
-    obs.send('TriggerHotkeyByName', { hotkeyName: 'OBSBasic.SelectedSourceScreenshot' })
-      .then(() => { })
-      .catch(errorHandler)
-      .finally(() => obs.disconnect());
-  }).catch(errorHandler);
+  obs
+    .connect(process.env.OBS_URL || '', process.env.OBS_PASSWORD || '')
+    .then(() => {
+      obs
+        .call('TriggerHotkeyByName', { hotkeyName: 'OBSBasic.SelectedSourceScreenshot' })
+        .then(() => {})
+        .catch(err => console.error(err))
+        .finally(() => obs.disconnect());
+    })
+    .catch(err => console.error(err));
 };
-web.conversations.setTopic({ channel: 'C03GZMPBD4M', topic: new Intl.DateTimeFormat('en-CA', { dateStyle: 'short', timeStyle: 'medium' }).format(Date.now()) })
 cron.schedule('*/10 * * * *', takeScreenshot, { scheduled: !0 });
