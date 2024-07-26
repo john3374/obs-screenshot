df4865013471e6c49b4ef9bc1377efed400d1ea5
John <john@akfn.net>
2024-07-26 00:03:55 -0400

    removed: node-cron added: readme

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
 require('dotenv').config();
-const cron = require('node-cron');
 const OBSWebSocket = require('obs-websocket-js').default;
-
-const takeScreenshot = () => {
-  const obs = new OBSWebSocket();
-  obs
-    .connect(process.env.OBS_URL || '', process.env.OBS_PASSWORD || '')
-    .then(() => {
-      obs
-        .call('TriggerHotkeyByName', { hotkeyName: 'OBSBasic.SelectedSourceScreenshot' })
-        .then(() => {})
-        .catch(err => console.error(err))
-        .finally(() => obs.disconnect());
-    })
-    .catch(err => console.error(err));
-};
-cron.schedule('*/10 * * * *', takeScreenshot, { scheduled: !0 });
+const obs = new OBSWebSocket();
+obs
+  .connect(process.env.OBS_URL || '', process.env.OBS_PASSWORD || '')
+  .then(() => {
+    obs
+      .call('TriggerHotkeyByName', { hotkeyName: 'OBSBasic.SelectedSourceScreenshot' })
+      .then(() => {})
+      .catch(err => console.error(err))
+      .finally(() => obs.disconnect());
+  })
+  .catch(err => console.error(err));
